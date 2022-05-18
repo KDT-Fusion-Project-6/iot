4abf8b979fadf94b6f03b2351c707f3f95299354
wjdtjddjq <tjddjq0114@naver.com>
2022-05-18 17:54:36 +0900

    220518 last

--- app/src/main/java/com/example/to_dy/MainActivity.kt
+++ app/src/main/java/com/example/to_dy/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.example.to_dy
+package com.example.viewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
